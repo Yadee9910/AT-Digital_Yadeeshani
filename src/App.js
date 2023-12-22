429395b5e7bd1b0fac1eabcbe5af43c615d37daf
Yadee9910 <syadeeshani@gmail.com>
2023-12-22 12:58:16 +0530

    feat:Footer with logo part was created and font was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Components/Button.js";
+/*import Button from "./Components/Button.js";*/
 /*import TopNavigation from "./Components/TopNavigation.js";*/
+import Footer from "./Components/Footer";
+
+
 
 function App() {
   return (
     <>
    
-   <Button /></>
+   <Footer /></>
     
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,20 @@
 import "../Styles/footer.css";
+import logo from "../Assests/Logo.jpg";
 
 function Footer(){
     return(
-        <>
-        </>
+        <div className="footer">
+            <div className="footer_logo">
+            <img className="Logo_img"
+            src={logo} alt=""/>
+            <p className="desc">
+            Your goal is our target. 
+            Not anything in between. We use online 
+            marketing platforms and tools to achieve single objective - your business results.
+            </p>
+            </div>
+
+        </div>
     );
 }
 export default Footer;
