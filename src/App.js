d43fbd95855d8b28ab313f6d784c82d2f68b6fe7
Yadee9910 <syadeeshani@gmail.com>
2023-12-22 19:26:28 +0530

    fix: button styles were changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/*import Button from "./Components/Button.js";*/
+import Button from "./Components/Button.js";
 import TopNavigation from "./Components/TopNavigation.js";
 /*import Footer from "./Components/Footer";*/
 
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
    
-   <TopNavigation /></>
+   <TopNavigation />
+   <Button />
+   </>
     
   );
 }
