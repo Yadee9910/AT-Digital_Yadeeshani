d715498aad334ab037336f14e2b6f0484c84824d
Yadee9910 <syadeeshani@gmail.com>
2023-12-22 16:18:57 +0530

    feat:technology and services were styled and created in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,6 +14,31 @@ function Footer(){
                     marketing platforms and tools to achieve single objective - your business results.
                     </p>
                 </div>
+
+                <div className="footer_desc">
+                    <div className="footer_tech">
+                        <h1 className="technology">Our Technologies</h1>
+                        <ul className="listitems">
+                            <li className="list">ReactJS</li>
+                            <li className="list">Gatsby</li>
+                            <li className="list">NextJS</li>
+                            <li className="list">NodeJS</li>
+                            <li className="list">GraphQL</li>
+                            <li className="list">Laravel</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer_service">
+                        <h1 className= "Services">Our Services</h1>
+                        <ul className="listitems">
+                            <li className="list">Social media Marketing</li>
+                            <li className="list">Web & Mobile App Development</li>
+                            <li className="list">Data & Analytics</li>
+                            <li className="list">Google Marketing solutions</li>
+                            <li className="list">Search Engine Optimization</li>
+                        </ul>
+                    </div>
+                </div>
            </div>
         </div>
         
