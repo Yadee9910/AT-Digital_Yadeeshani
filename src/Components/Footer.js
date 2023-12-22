19effcf79252c4ae98b47dc210b2e299fa05b4f3
Yadee9910 <syadeeshani@gmail.com>
2023-12-22 13:57:04 +0530

    fix: The foter layout was fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,17 +4,19 @@ import logo from "../Assests/Logo.jpg";
 function Footer(){
     return(
         <div className="footer">
-            <div className="footer_logo">
-            <img className="Logo_img"
-            src={logo} alt=""/>
-            <p className="desc">
-            Your goal is our target. 
-            Not anything in between. We use online 
-            marketing platforms and tools to achieve single objective - your business results.
-            </p>
-            </div>
-
+           <div className="footer_container">
+                <div className="footer_logo">
+                    <img className="Logo_img"
+                    src={logo} alt=""/>
+                    <p className="desc">
+                    Your goal is our target. 
+                    Not anything in between. We use online 
+                    marketing platforms and tools to achieve single objective - your business results.
+                    </p>
+                </div>
+           </div>
         </div>
+        
     );
 }
 export default Footer;
