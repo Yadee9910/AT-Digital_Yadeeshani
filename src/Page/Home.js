e4d96d7a746db8be086053c02a3cea9f8bc2a4db
Yadee9910 <syadeeshani@gmail.com>
2023-12-23 01:51:51 +0530

    feat: created the digital content part

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -20,17 +20,33 @@ function Home(){
     <div className="home_contentweb">
         <div className="home_webcontent_img">
         </div>
-        <div className="web_content">
-            <h1 className="web_topic">Web & Mobile App Development</h1>
-            <p className="content_home">
-            Your web and mobile Apps are pieces of the puzzle to grow your 
-            business. We use frameworks which tailor content and engagement 
-            methods to respond to different intents shown by your potential 
-            customers who interact with your business online.
-            </p>
-            <Button text="Learn more" style="btn_viewmore" />
-        </div>
+            <div className="web_content">
+                <h1 className="web_topic">Web & Mobile App Development</h1>
+                <p className="content_home">
+                Your web and mobile Apps are pieces of the puzzle to grow your 
+                business. We use frameworks which tailor content and engagement 
+                methods to respond to different intents shown by your potential 
+                customers who interact with your business online.
+                </p>
+                <Button text="Learn more" style="btn_viewmore" />
+            </div>
+    </div>
+
+    <div className="home_contentDigital">
+        <div className="Digital_content">
+                <h1 className="Digital_topic">Digital Strategy Consulting</h1>
+                <p className="content_home">
+                Your digital strategy should complement the overall marketing 
+                strategy of the company. In online marketing, each component 
+                will never work in isolation and every business needs a different mix. 
+                We provide a clear concept and strategic overview to find the most efficient model for your business.
+                </p>
+                <Button text="Learn more" style="btn_viewmore" />
+            </div>
         
+        <div className="home_Digitalcontent_img">
+        </div>
+
     </div>
 </div>
 
